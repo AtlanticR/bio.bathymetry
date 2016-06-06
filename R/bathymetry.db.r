2493d42550f045176dbb3f92048cd91c0ea83c66
Jae S Choi <choijaeseok@gmail.com>
2016-06-06 09:51:36 -0300

    rename projects

--- R/bathymetry.db.r
+++ R/bathymetry.db.r
@@ -177,8 +177,7 @@
 
 			if ( "snowcrab" %in% additional.data ) {
         # range from 23.8 to 408 m below sea level ... these have dropped the "-" for below sea level; n=5925 (in 2014)
-			  # p0 = p  # loadfunctions "snowcrab" will overwrite p .. store a copy and return it to original state below
-        bioLibrary( "snowcrab")
+        bioLibrary( "bio.snowcrab")
         sc = snowcrab.db("set.clean")[,c("lon", "lat", "z") ]
 				sc = sc [ which (is.finite( rowSums( sc ) ) ) ,]
 				j = which(duplicated(sc))
@@ -441,12 +440,12 @@
      # fishable biomass density
 
       RLibrary( "mgcv", "chron", "lattice" )
-      bioLibrary( "snowcrab")
+      bioLibrary( "bio.snowcrab")
 
       gmt = initialise.local.environment()
 
       gmt = list()
-      gmt$out = file.path( project.datadirectory("snowcrab"), "maps", "googleearth", "R0.platecarre.ps" )
+      gmt$out = file.path( project.datadirectory("bio.snowcrab"), "maps", "googleearth", "R0.platecarre.ps" )
       gmt$outputs = c( "colourscale", "colourcontour" )
       gmt$region=" -R-72/-52/40/50"
       gmt$gmtproj="-JQ-62/6.5i"  # Cylindrical equidistant (Plate Carre)  the default required by Google Earth
@@ -486,12 +485,12 @@
      # SD in fishable biomass
 
       RLibrary( "mgcv", "chron", "lattice" )
-      bioLibrary( "snowcrab" )
+      bioLibrary( "bio.snowcrab" )
 
       gmt = initialise.local.environment()
 
       gmt = list()
-      gmt$out = file.path( project.datadirectory("snowcrab"), "maps", "googleearth", "R0.sd.platecarre.ps" )
+      gmt$out = file.path( project.datadirectory("bio.snowcrab"), "maps", "googleearth", "R0.sd.platecarre.ps" )
       gmt$outputs = c( "colourscale", "colourcontour" )
       gmt$region=" -R-72/-52/40/50"
       gmt$gmtproj="-JQ-62/6.5i"  # Cylindrical equidistant (Plate Carre)  the default required by Google Earth
