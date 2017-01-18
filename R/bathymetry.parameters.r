5d0be05ef497b2b7f84fd63f0a8fbdf01006f831
Jae S Choi <choijaeseok@gmail.com>
2017-01-18 12:55:02 -0400

    misc

--- R/bathymetry.parameters.r
+++ R/bathymetry.parameters.r
@@ -39,10 +39,10 @@ bathymetry.parameters = function(DS="bio.bathymetry", p=NULL, resolution=NULL ) 
     p$lbm_distance_max = 50 # never go beyond the min and max range ( cpu/ram and time consideration are part of it but mostly what is physically reasonable)
 
     
-    p$n.min = 100 # n.min/n.max changes with resolution
-    p$n.max = 7500 # numerical time/memory constraint -- anything larger takes too much time
+    p$n.min = 200 # n.min/n.max changes with resolution
+    p$n.max = 5000 # numerical time/memory constraint -- anything larger takes too much time
     # other options might work depending upon data density but GP are esp slow .. too slow for bathymetry
-    p$sampling = c( 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2, 1.5, 1.75, 2 )  # fractions of median distance scale to try in local block search
+    p$sampling = c( 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.1, 1.2, 1.5 )  # fractions of median distance scale to try in local block search
  
     p$variables = list( Y="z", LOCS=c("plon", "plat") ) 
 
